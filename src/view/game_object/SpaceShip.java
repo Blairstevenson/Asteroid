b69fbed5e40a5d905395b07d7c0a0984f8a2d45b
phuocdh90 <phuocdh90@gmail.com>
2015-03-07 12:15:31 +0700

    Start building advanced version.

--- src/view/game_object/SpaceShip.java
+++ src/view/game_object/SpaceShip.java
@@ -5,6 +5,7 @@ import java.awt.Graphics;
 
 public class SpaceShip extends BaseGameObject {
 
+    private static final int radian = 4;
     private double facingAngle;
 
     public SpaceShip() {
@@ -16,15 +17,36 @@ public class SpaceShip extends BaseGameObject {
 
     @Override
     public void draw(Graphics g) {
+        // draw inner circle
+        g.setColor(Color.yellow);
+        g.drawOval(this.getPosx() - SpaceShip.radian,
+                this.getPosy() - SpaceShip.radian,
+                SpaceShip.radian * 2,
+                SpaceShip.radian * 2);
+        // draw pointer line
+        int[] pointStart = SpaceShip.rotate(radian, 0,
+                this.getPosx(), this.getPosy(), this.facingAngle);
+        int[] pointEnd = SpaceShip.rotate(this.getBound(), 0,
+                this.getPosx(), this.getPosy(), this.facingAngle);
+        g.drawLine(pointStart[0], pointStart[1],
+                pointEnd[0], pointEnd[1]);
+        // draw outer line
         g.setColor(Color.red);
-        g.drawOval(this.getPosx() - this.getBound(),
-                this.getPosy() - this.getBound(),
-                this.getBound() * 2, this.getBound() * 2);
-        // draw pointer
-        g.drawLine((int) (this.getPosx() + this.getBound() * Math.cos(this.facingAngle)),
-                (int) (this.getPosy() + this.getBound() * Math.sin(this.facingAngle)),
-                (int) (this.getPosx() + this.getBound() * 1.5 * Math.cos(this.facingAngle)),
-                (int) (this.getPosy() + this.getBound() * 1.5 * Math.sin(this.facingAngle)));
+        int[] outerMid = SpaceShip.rotate((int) (this.getBound() * 1.5), 0,
+                this.getPosx(), this.getPosy(),
+                this.facingAngle);
+        int[] outerLeft = SpaceShip.rotate((int) (-this.getBound() * 0.67),
+                -this.getBound(),
+                this.getPosx(), this.getPosy(), this.facingAngle);
+        int[] outerRight = SpaceShip.rotate((int) (-this.getBound() * 0.67),
+                this.getBound(),
+                this.getPosx(), this.getPosy(), this.facingAngle);
+        g.drawLine(outerMid[0], outerMid[1],
+                outerLeft[0], outerLeft[1]);
+        g.drawLine(outerMid[0], outerMid[1],
+                outerRight[0], outerRight[1]);
+        g.drawLine(outerLeft[0], outerLeft[1],
+                outerRight[0], outerRight[1]);
     }
 
     public double getFacingAngle() {
@@ -34,14 +56,14 @@ public class SpaceShip extends BaseGameObject {
     public void setFacingAngle(double facingAngle) {
         this.facingAngle = facingAngle;
     }
-    
+
     public void moveForward(int step) {
         int[] newPoint = BaseGameObject.rotate(step, 0,
                 this.getPosx(), this.getPosy(),
                 this.facingAngle);
         this.setPosition(newPoint[0], newPoint[1]);
     }
-    
+
     public void moveBackward(int step) {
         int[] newPoint = BaseGameObject.rotate(-step, 0,
                 this.getPosx(), this.getPosy(),
